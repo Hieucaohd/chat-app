ed29bc9e53ed272f7f989923b6b47ba84c75c15b
hieucaohd <hieucaohd@gmail.com>
2021-10-18 14:54:06 +0700

    viet file script

--- index.js
+++ index.js
@@ -6,38 +6,6 @@ const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 
-const DEBUG = true;
-
-let options = {};
-if (DEBUG) {
-	options = {
-		cors: {
-			origin: ["http://localhost:3000", 
-					 "http://192.168.29.102:3000", 
-					 "https://timgiasu.vercel.app", 
-					 "http://192.168.1.18:3000"],
-			methods: ["GET", "POST"], 
-		}
-	} 
-} else {
-	options = {
-		cors: {
-			origin: ["https://timgiasu.vercel.app"],
-			methods: ["GET", "POST"], 
-		}
-	} 
-}
-
-const io = require('socket.io')(server, options)
-
-io.on("connection", (socket) => {
-	console.log("A user is connected");
-
-	socket.on("disconnect", () => {
-		console.log("A user is disconnect");
-	})
-})
-
 server.listen(PORT, () => {
 	console.log(`server is running on ${PORT}`);
 })
---
+++ scripts/application_start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# restart the supervisor run chat app
+supervisorctl restart findTutor:findTutorChatApp
